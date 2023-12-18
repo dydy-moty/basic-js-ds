2c34c62353b0c904bae39ea9e4b5d75b3ba7dd1d
Matvey Ishmametev <ishmametev_m@mail.ru>
2023-12-18 20:02:08 +0500

    feat: add methods root, add

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,21 +1,40 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
-
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor( ) {
+    this.rootVar = null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  root() {
+    return this.rootVar;
+  }
+
+  add(data) {
+    this.rootVar = addElement(this.rootVar, data);
+
+    function addElement(node, data) {
+      if (!node) {
+        return new Node(data);
+      }
+
+      if (node.data === data) {
+        return node;
+      }
+
+      if (data < node.data) {
+        node.left = addElement(node.left, data);
+      } else {
+        node.right = addElement(node.right, data)
+      }
+
+      return node;
+    }
   }
 
   has(/* data */) {
